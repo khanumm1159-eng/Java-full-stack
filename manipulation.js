39b39704c3e7ec1732159f3d230daf09afc77903
khanumm1159-eng <khanumm1159@gmail.com>
2026-06-13 11:50:12 +0530

    Add files via upload

---
+++ manipulation.js
@@ -0,0 +1,11 @@
+
+window.onload = function () {
+    alert("🎉 Happy Birthday! Have a wonderful day! 🎂");
+};
+
+// Change heading color when clicked
+const heading = document.querySelector("h1");
+
+heading.addEventListener("click", function () {
+    heading.style.color = "gold";
+});
